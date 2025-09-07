71417fa072a584166be5b7c886d80fccb36c38bf
jxrdysob <samuelbermudez912@gmail.com>
2025-09-07 11:31:32 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🔬 Mi App de Química</h1>
+      <p>Hola Jordy, tu aplicación ya funciona 🎉</p>
+    </div>
+  );
+}
